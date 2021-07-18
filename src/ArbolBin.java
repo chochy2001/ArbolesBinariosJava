a8018c42945403f7739a3ad186dd77e25be5195c
Jorge Salgado Miranda <ohchochy@gmail,com>
2021-07-18 13:57:08 -0500

    Primer Commit de la practica

---
+++ src/ArbolBin.java
@@ -0,0 +1,54 @@
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ *
+ * @author Jorge Salgado Miranda
+ * @version 1.0.0
+ *
+ */
+
+public class ArbolBin {
+    Nodo root;
+    
+    public ArbolBin(){
+        root=null;
+    }
+    
+    public ArbolBin(int val){
+        root=new Nodo(val);
+    }
+    
+    public ArbolBin(Nodo root){
+        this.root=root;
+    }
+    
+    public void add(Nodo padre, Nodo hijo, int lado){
+	if(lado==0)
+            padre.setIzq(hijo);
+	else
+            padre.setDer(hijo);
+    }
+    
+    protected void visit(Nodo n){
+        System.out.println(n.valor+" ");
+    }	
+    
+    public void breadthFirst(){
+        Nodo r = root;
+	Queue<Nodo> queue = new LinkedList();
+	if(r!=null){
+            queue.add(r);
+            while(!queue.isEmpty()){
+                r = (Nodo)queue.poll();
+		visit(r);
+		if(r.izq!=null)
+                    queue.add(r.izq);
+		if(r.der!=null)
+		queue.add(r.der);
+            }
+	}
+    }
+
+}
